08953e4659dd0e293ce088d01f05b83f64fd25e8
theking9985 <theking9985@yahoo.com>
2020-10-06 13:40:55 -0700

    add search button

--- src/frontend/components/search/Search.tsx
+++ src/frontend/components/search/Search.tsx
@@ -1,24 +1,34 @@
-import React, { useState, FormEvent } from 'react';
-import { useDispatch } from 'react-redux';
-import styled from 'styled-components';
-import { searchWordSubmitted } from './searchSlice';
+import React, { useState, FormEvent } from 'react'
+import { useDispatch } from 'react-redux'
+import styled from 'styled-components'
+import { searchWordSubmitted } from './searchSlice'
 
 const SearchContainer = styled.div`
   padding: 0 20px;
 
-  input {
-    width: 100%;
+  form {
+    display: flex;
   }
-`;
+
+  input {
+    width: 75%;
+    margin-right: 5%;
+  }
+
+  button {
+    width: 20%;
+  }
+`
 
 export default function Search() {
-  const [value, setValue] = useState('');
-  const dispatch = useDispatch();
+  const [value, setValue] = useState('')
+  const dispatch = useDispatch()
+  const disabled: boolean = !value.length
 
   const handleSubmit = (e: any) => {
-    e.preventDefault();
-    dispatch(searchWordSubmitted({ value }));
-  };
+    e.preventDefault()
+    dispatch(searchWordSubmitted({ value }))
+  }
 
   return (
     <SearchContainer>
@@ -29,7 +39,10 @@ export default function Search() {
           placeholder="Search a stock symbol"
           onChange={(e) => setValue(e.target.value)}
         />
+        <button type="button" disabled={disabled} onClick={handleSubmit}>
+          Search
+        </button>
       </form>
     </SearchContainer>
-  );
+  )
 }
