4deaf1bbd394108cd7bdc9cf82d431cce8369ad0
theking9985 <theking9985@yahoo.com>
2020-10-06 20:22:40 -0700

    fix test error

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { Provider } from 'react-redux'
 import { App } from './App'
-import store from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
