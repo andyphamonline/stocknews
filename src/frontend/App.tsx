a2783e497bc13c49e6d3e9683163085b5bcbe949
theking9985 <theking9985@yahoo.com>
2020-10-06 20:47:49 -0700

    add redux-saga and fix app.test.tsx

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Provider } from 'react-redux'
-import store from './store/store'
 import { Header } from './components/Header'
 import { TickerBoard } from './components/TickerBoard'
 import { WatchList } from './components/WatchList'
@@ -11,16 +9,13 @@ const AppContainer = styled.div`
   padding: 0 20px;
 `
 
-// TO DO: move <Provider> to index.tsx, rewrite App.test.tsx to make it work
 export const App = () => {
   return (
-    <Provider store={store}>
-      <AppContainer>
-        <Header />
-        <TickerBoard />
-        <WatchList />
-        <NewsFeed />
-      </AppContainer>
-    </Provider>
+    <AppContainer>
+      <Header />
+      <TickerBoard />
+      <WatchList />
+      <NewsFeed />
+    </AppContainer>
   )
 }
